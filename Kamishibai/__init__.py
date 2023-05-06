3748a9890a68020fc672494ad20c99846588bea6
UenoTakaaki <s1f102002097@iniad.org>
2023-05-07 02:53:37 +0900

    D&DとINDEX　どっちに挙げても同じになったやで

--- Kamishibai/__init__.py
+++ Kamishibai/__init__.py
@@ -1,53 +1,87 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
+from IPython.display import Audio, display
 import os
 
 app = Flask(__name__)
 
-# 画像ファイルが保存されるディレクトリを指定
-UPLOAD_IMAGES_FOLDER = 'Kamishibai/static/images'
-app.config['UPLOAD_IMAGES_FOLDER'] = UPLOAD_IMAGES_FOLDER
+# アップロードされた画像が保存されるディレクトリを指定
+UPLOAD_IMAGE_FOLDER = 'Kamishibai/static/images'
+app.config['UPLOAD_IMAGE_FOLDER'] = UPLOAD_IMAGE_FOLDER
+# アップロードされた音声が保存されるディレクトリを指定
+UPLOAD_AUDIO_FOLDER = 'Kamishibai/static/audios'
+app.config['UPLOAD_AUDIO_FOLDER'] = UPLOAD_AUDIO_FOLDER
 
-# 音声ファイルが保存されるディレクトリを指定
-UPLOAD_AUDIOS_FOLDER = 'Kamishibai/static/audios'
-app.config['UPLOAD_AUDIOS_FOLDER'] = UPLOAD_AUDIOS_FOLDER
+# アップロードを許可するファイルの拡張子を定義
+ALLOWED_IMAGE_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+ALLOWED_AUDIO_EXTENSIONS = {'mp3', 'wav'}
 
-# アップロードを許可するファイルの拡張子を指定
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'mp4', 'wav', 'mp3'}
 
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+# 画像ファイルの一覧を取得する関数を定義
+def get_image_list():
+    images = []
+    for file_name in os.listdir(UPLOAD_IMAGE_FOLDER):
+        if file_name.endswith(tuple(ALLOWED_IMAGE_EXTENSIONS)):
+            images.append(file_name)
+    return images
+# 音声ファイルの一覧を取得する関数を定義
+def get_audio_list():
+    audios = []
+    for file_name in os.listdir(UPLOAD_AUDIO_FOLDER):
+        if file_name.endswith(tuple(ALLOWED_AUDIO_EXTENSIONS)):
+            audios.append(file_name)
+    return audios
 
-def get_images():
-    # 画像ファイルの一覧を取得
-    images = [f for f in os.listdir(app.config['UPLOAD_IMAGES_FOLDER']) if allowed_file(f)]
-    # 音声ファイルの一覧を取得
-    audios = [f for f in os.listdir(app.config['UPLOAD_AUDIOS_FOLDER']) if allowed_file(f)]
-    return images, audios
 
+# トップページのエンドポイントを定義
 @app.route('/')
 def index():
-    images, _ = get_images()
-    return render_template('home.html', images=images)
+    images = get_image_list()
+    audios = get_audio_list()
+    #display_audio_list()
+    # HTMLに変数として要素を渡す。
+    return render_template('home.html', images=images, audios=audios,get_audio_type=get_audio_type)
 
-@app.route('/upload', methods=['POST'])
-def upload():
-    # アップロードされたファイルを保存
+
+# 画像をアップロードするエンドポイントを定義
+@app.route('/upload_image', methods=['POST'])
+def upload_image():
     file = request.files['file']
-    if not file or not allowed_file(file.filename):
-        flash('無効なファイル形式です。')
-        return '''
-        <script>
-        alert("無効なファイル形式です。");
-        window.location.href = "/";
-        </script>
-        '''
     filename = file.filename
-    if filename.endswith(('mp4', 'wav', 'mp3')):
-        file.save(os.path.join(app.config['UPLOAD_AUDIOS_FOLDER'], filename))
+    # ファイルが許可された拡張子であるか確認する
+    if '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_IMAGE_EXTENSIONS:
+        file.save(os.path.join(app.config['UPLOAD_IMAGE_FOLDER'], filename))
     else:
-        file.save(os.path.join(app.config['UPLOAD_IMAGES_FOLDER'], filename))
+        flash('無効なファイル形式です。')
     return redirect(url_for('index'))
+# 音声ファイルをアップロードするエンドポイントを定義
+@app.route('/upload_audio', methods=['POST'])
+def upload_audio():
+    file = request.files['file']
+    filename = file.filename
+    # ファイルが許可された拡張子であるか確認する
+    if '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_AUDIO_EXTENSIONS:
+        file.save(os.path.join(app.config['UPLOAD_AUDIO_FOLDER'], filename))
+    else:
+        flash('無効なファイル形式です。')
+    return redirect(url_for('index'))
+
+
+##　音声用の追加関数　##
+
+#　音声再生用
+# def display_audio_list():
+#     audio_list = get_audio_list()
+#     for file_name in audio_list:
+#         file_path = os.path.join(UPLOAD_AUDIO_FOLDER, file_name)
+#         display(Audio(file_path, autoplay=False))
+#         # autoplayで自動再生を防いでいます
+#　音声拡張子フィルター
+def get_audio_type(filename):
+    _, ext = os.path.splitext(filename)
+    if ext.lower()[1:] in ALLOWED_AUDIO_EXTENSIONS:
+        return ext.lower()[1:]
+    else:
+        return None
 
 if __name__ == '__main__':
     app.run(debug=True)
--- Kamishibai/static/imgUpload.js
+++ Kamishibai/static/imgUpload.js
@@ -1,6 +1,7 @@
 $(function() {
     var dropArea = $('.drop-area');
-            // ドラッグ&ドロップされたときの処理
+    
+// ドラッグ&ドロップされたときの処理
 dropArea.on('dragover', function(e) {
     // デフォルト処理を停止
     e.stopPropagation();
@@ -24,8 +25,12 @@ dropArea.on('drop', function(e) {
     var formData = new FormData();
     formData.append('file', files[0]);
 
+    console.log('formData:', formData);
+    console.log('files:', files);
+
+
     $.ajax({
-        url: '/upload',
+        url: '/upload_audio',
         type: 'POST',
         data: formData,
         contentType: false,
@@ -37,6 +42,21 @@ dropArea.on('drop', function(e) {
             alert('アップロードに失敗しました。');
         }
     });
+
+    $.ajax({
+        url: '/upload_image',
+        type: 'POST',
+        data: formData,
+        contentType: false,
+        processData: false,
+        success: function() {
+            location.reload();
+        },
+        error: function() {
+            alert('アップロードに失敗しました。');
+        }
+    });
+});
 });
 
 // ファイル選択されたときの処理
@@ -45,7 +65,7 @@ fileInput.on('change', function() {
     var formData = new FormData($('#upload-form')[0]);
 
     $.ajax({
-        url: '/upload',
+        url: '/upload_audio',
         type: 'POST',
         data: formData,
         contentType: false,
@@ -58,4 +78,52 @@ fileInput.on('change', function() {
         }
     });
 });
-});
+
+window.onload = function() {
+	// ul要素を取得する
+	var audioList = document.getElementById("audioList");
+
+	// audiosフォルダ内の音声ファイルを取得する
+	fetch("audios/")
+	.then(response => response.text())
+	.then(data => {
+		// 取得したデータをHTMLに変換する
+		var parser = new DOMParser();
+		var html = parser.parseFromString(data, "text/html");
+
+		// HTMLからリンク要素を取得する
+		var links = html.getElementsByTagName("a");
+
+		// リンク要素を順に処理する
+		for(var i = 0; i < links.length; i++) {
+			var link = links[i];
+
+			// 音声ファイルのみを処理する
+			if(link.href.endsWith(".mp3") || link.href.endsWith(".wav")) {
+				// li要素を作成する
+				var li = document.createElement("li");
+
+				// a要素を作成する
+				var a = document.createElement("a");
+				a.href = link.href;
+				a.innerHTML = link.innerHTML;
+				a.onclick = function() {
+					// 音声ファイルを再生する
+					var audio = new Audio(this.href);
+					audio.play();
+					return false;
+				}
+
+				// li要素にa要素を追加する
+				li.appendChild(a);
+
+				// ul要素にli要素を追加する
+				audioList.appendChild(li);
+			}
+		}
+	})
+	.catch(error => {
+		console.error("Error:", error);
+	});
+};
+
